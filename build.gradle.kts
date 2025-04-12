13a5e930b0f10d80aae04ee715e8df0d88c9f707
Alexandra <molotkovaa@list.ru>
2025-04-12 15:51:59 +0300

    change build.gradle.kts for jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     java
     id("org.springframework.boot") version "2.7.18"
     id("io.spring.dependency-management") version "1.1.7"
+    id("application")
 }
+
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -15,6 +18,19 @@ dependencies {
     testImplementation("org.mockito:mockito-junit-jupiter")
 }
 
+application {
+    mainClass.set("neoflex.VacationPayCalculatorApplication")
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveBaseName.set("vacation-pay-calculator")
+    archiveVersion.set("1.0")
+    archiveClassifier.set("")
+    manifest {
+        attributes("Main-Class" to "neoflex.VacationPayCalculatorApplication")
+    }
 }
